6b59b99d17080e52a778443ffab7cbfb1f8e6792
Vinoth G <vin10catchme@gmail.com>
2019-02-02 21:49:28 +0530

    Message Bean with mail controller

---
+++ src/main/java/com/mailer/controller/MailController.java
@@ -0,0 +1,31 @@
+package com.mailer.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mailer.domain.Message;
+import com.mailer.producer.MessageProducer;
+
+@RestController
+public class MailController {
+
+	private Logger logger = LoggerFactory.getLogger(MailController.class);
+	@Autowired
+	private MessageProducer messageProducer;
+
+	@RequestMapping(value = "/send", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.ACCEPTED)
+	public String send(@RequestBody Message message) {
+		messageProducer.sendMessage(message);
+		logger.info("Received mail details in rest call");
+		System.out.println("Received message in rest:" + message);
+		return "Request Accepted!";
+	}
+}
---
+++ src/main/java/com/mailer/domain/Message.java
@@ -0,0 +1,71 @@
+package com.mailer.domain;
+
+import java.net.URI;
+
+public class Message {
+	private String from;
+	private String to;
+	private String subject;
+	private String body;
+	private URI uri;
+	/**
+	 * @return the from
+	 */
+	public String getFrom() {
+		return from;
+	}
+	/**
+	 * @param from the from to set
+	 */
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	/**
+	 * @return the to
+	 */
+	public String getTo() {
+		return to;
+	}
+	/**
+	 * @param to the to to set
+	 */
+	public void setTo(String to) {
+		this.to = to;
+	}
+	/**
+	 * @return the subject
+	 */
+	public String getSubject() {
+		return subject;
+	}
+	/**
+	 * @param subject the subject to set
+	 */
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	/**
+	 * @return the body
+	 */
+	public String getBody() {
+		return body;
+	}
+	/**
+	 * @param body the body to set
+	 */
+	public void setBody(String body) {
+		this.body = body;
+	}
+	/**
+	 * @return the uri
+	 */
+	public URI getUri() {
+		return uri;
+	}
+	/**
+	 * @param uri the uri to set
+	 */
+	public void setUri(URI uri) {
+		this.uri = uri;
+	}
+}
